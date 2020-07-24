2c48b921fb90eb5cfc9817880e3fcaaa5e99e4aa
Bunea Rares <bunearaes@gmail.com>
2020-07-25 01:44:56 +0300

    ???

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-  const gameBoard = document.querySelector(`#game-board`);
+  const board = document.querySelector(`#game-board`);
   const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -10,24 +10,74 @@ const gameBoard = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  const player1 = `x`;
+  const player2 = `0`;
+  let round = 0;
+  let winPlayer1 = [];
+  let winPlayer2 = [];
   const createGrid = () => {
-    const tr = document.createElement(`tr`);
-    const td = document.createElement("td");
     let count = 0;
-    tr.className = `t-row`;
     for (let i = 0; i < 3; i++) {
-      for (let j = 0; i < 3; j++) {
-        td.className = `data-index-${count}`;
+      let tr = document.createElement(`tr`);
+      for (let j = 0; j < 3; j++) {
+        let td = document.createElement("td");
+        td.id = `data-index-${count}`;
         tr.append(td);
         count++;
+        console.log(td);
       }
-      gameBoard.append(tr);
+      board.append(tr);
     }
   };
-  init = () => {
+  const init = () => {
     createGrid();
+    clickEvent();
   };
-  return { init };
+  const clickEvent = () => {
+    let gridGet = document.querySelectorAll(`td`);
+    gridGet.forEach((e) => {
+      e.addEventListener(`click`, checkPlayer);
+    });
+  };
+  const checkPlayer = (e) => {
+    let id = e.target.id;
+    let getIdItem = document.querySelector(`#${id}`);
+    if (round % 2 === 0) {
+      if (getIdItem.innerHTML == ``) {
+        getIdItem.innerHTML = player1;
+        round++;
+        getPlayerArr(e);
+      }
+    } else {
+      if (getIdItem.innerHTML == ``) {
+        getIdItem.innerHTML = player2;
+        round++;
+        getPlayerArr(e);
+      }
+    }
+    if (round >= 9) {
+      round = 0;
+    }
+  };
+  const getPlayerArr = (e) => {
+    let id = e.target.id;
+    let content = e.target.textContent;
+    if (content === `x`) {
+      winPlayer1.push(id.slice(id.length - 1));
+      if (checkPlayerWin(winPlayer1, winningConditions)) {
+        prompt(`won`);
+      }
+    } else {
+      winPlayer2.push(id.slice(id.length - 1));
+      if (checkPlayerWin(winPlayer2, winningConditions)) {
+        prompt(`won`);
+      }
+    }
+  };
+  const checkPlayerWin = (playerCon, winningConditions) => {
+    return !!winningConditions.find(
+      (_playerCon) => _playerCon.join() === playerCon.join()
+    );
+  };
+  init();
 })();
-
-init();
